9e80616ae64239a6e469771eb4443d6344b31e71
Michael Carter <CarterMichael@gmail.com = Michael Carter CarterMichael@gmail.com>
2009-10-05 20:51:28 +0000

    increment the pyjsiocompile version and release

--- compilers/pyjsiocompile/setup.py
+++ compilers/pyjsiocompile/setup.py
@@ -6,7 +6,7 @@ import os, sys
 
 setup(
     name='pyjsiocompile',
-    version='0.1a3',
+    version='0.1a4',
     author='Michael Carter',
     author_email='CarterMichael@gmail.com',
     license='MIT License',
