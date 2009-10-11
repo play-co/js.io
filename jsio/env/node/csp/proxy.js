ce0759ed923f5e654033180e41f20ee23f90cce4
Jacob Rus <jacob.rus@gmail.com = Jacob Rus jacob.rus@gmail.com>
2009-10-11 12:52:27 +0000

    convert to martin's class system. also, convert spaces → tabs

--- jsio/env/node/csp/proxy.js
+++ jsio/env/node/csp/proxy.js
@@ -35,123 +35,127 @@ jsio('from .server import csp')
 // msp = "Multiplexing Socket Proxy"
 var msp = this.msp = {};
 ;(function () {
-  frameTypes = ['open', 'close', 'data'];
-  var errorCodes = {
-    InvalidHandshake: 102,
-    UserConnectionReset: 103,
-    RemoteConnectionTimeout: 104,
-    Unauthorized: 106,
-    RemoteConnectionFailed: 108,
-    RemoteConnectionClosed: 109,
-    ProtocolError: 110,
-  };
-  var FRAME = {
-    OPENED: 0,
-    CLOSED: 1,
-    DATA: 2,
-  };
-  msp.ProxyConnection = function (inConnection) {
-    var inBuffer = '';
-    var outConnections = {};
-    var receiveData = function (data) {
-      var frameBegin;
-      inBuffer += data;
-      try {
-        while ((frameBegin = inBuffer.indexOf('[')) != -1) {
-          var frameEnd = parseInt(inBuffer.slice(0, frameBegin)) + frameBegin;
-          assert (!isNaN(frameEnd), 'Invalid frame size prefix');
-          if (inBuffer.length < frameEnd) {
-            break; // whole frame hasn't arrived yet
-          }
-          var frame = JSON.parse(inBuffer.slice(frameBegin, frameEnd));
-          inBuffer = inBuffer.slice(frameEnd) // remove frame from buffer
-          // frame consists of connection id, frame type, arbitrary other arguments
-          assert(frame instanceof Array && frame.length >= 2, 'Invalid frame');
-          var connectionId = frame.shift();
-          var frameType = frameTypes[frame.shift()];
-          assert(frameType, 'Unrecognized frame type');
-          var args = frame; args.unshift(connectionId); // put back connection ID
-          dispatchFrame[frameType].apply(this, args);
-        };
-      } catch (e) {
-        debug('PROTOCOL ERROR: ', (e.message || 'unknown error'));
-        shutdown(true);
-      };
-    };
-    var shutdown = function (had_error) {
-      // close all outgoing TCP connections      
-      for (connectionId in outConnections) {
-        if (had_error) {
-          closeOutgoing(connectionId, 'ProtocolError');
-        } else {
-          closeOutgoing(connectionId, 'UserConnectionReset');
-        }
-        outConnections[connectionId].listeners('connect') = [];
-        outConnections[connectionId].listeners('receive') = [];
-        outConnections[connectionId].listeners('eof') = [];
-        outConnections[connectionId].listeners('disconnect') = [];
-      };
-    };
-    inConnection
-      .addListener('receive', receiveData)
-      .addListener('eof', shutdown)
-      .addListener('disconnect', shutdown)
-      .setEncoding('bytes');
-    var send = function (frame) {
-      payload = JSON.stringify(frame);
-      inConnection.send(payload.length + ',' + payload, 'bytes');
-    };
-    var closeOutgoing = function (connectionId, errorType) {
-      if (connectionId in outConnections) {
-        var code = errorCodes[errorType];
-        send([connectionId, FRAME.CLOSED, code]);
-        outConnections[connectionId].close();
-        delete outConnections[connectionId];
-      };
-    };
-    var dispatchFrame = {
-      open: function (connectionId, host, port) {
-        assert(!(connectionId in outConnections), 'OPEN frame for existing connection');
-        assert(host && port, 'Invalid host or port');
-        var outConn = outConnections[connectionId] = node.tcp.createConnection(port, host);
-        outConn.setEncoding('bytes');
-        outConn
-          .addListener('connect', function () {
-            send([connectionId, FRAME.OPENED]);
-          })
-          .addListener('receive', function (data) {
-            send([connectionId, FRAME.DATA, data]);
-          })
-          .addListener('eof', function () {
-            closeOutgoing(connectionId, 'RemoteConnectionClosed');
-          })
-          .addListener('disconnect', function (had_error) {
-            closeOutgoing(connectionId, 'RemoteConnectionClosed');
-          });
-      },
-      close: function (connectionId) {
-        closeOutgoing(connectionId, 'UserConnectionReset');
-      },
-      data: function (connectionId, data) {
-        data = unescape(data)
-        outConnections[connectionId].send(data, 'bytes');
-      },
-    };
-  };
-  msp.Proxy = function () {
-    var self = this;
-    self.cspserver = csp.createServer(function(connection) {
-      proxyConnection = new msp.ProxyConnection(connection);
-    });
-    self.listen = function (port, host) {
-      self.cspserver.listen(port, host);
-    };
-  };
-})();
+
+var frameTypes = ['open', 'close', 'data'];
+var FRAME_OPENED = 0, FRAME_CLOSED = 1, FRAME_DATA = 2;
+var errorCodes = {
+	InvalidHandshake: 102,
+	UserConnectionReset: 103,
+	RemoteConnectionTimeout: 104,
+	Unauthorized: 106,
+	RemoteConnectionFailed: 108,
+	RemoteConnectionClosed: 109,
+	ProtocolError: 110,
+};
+msp.ProxyConnection = Class(function() {
+	this.init = function (inConnection) {
+		this.inConnection = inConnection;
+		this.inBuffer = '';
+		this.outConnections = {};
+		this.inConnection
+			.addListener('receive', bind(this, this.receiveData))
+			.addListener('eof', bind(this, this.shutdown))
+			.addListener('disconnect', bind(this, this.shutdown))
+			.setEncoding('bytes');	
+	};
+	this.receiveData = function (data) {
+		var frameBegin;
+		this.inBuffer += data;
+		try {
+			while ((frameBegin = this.inBuffer.indexOf('[')) != -1) {
+				var frameEnd = parseInt(this.inBuffer.slice(0, frameBegin)) + frameBegin;
+				assert (!isNaN(frameEnd), 'Invalid frame size prefix');
+				if (this.inBuffer.length < frameEnd) {
+					break; // whole frame hasn't arrived yet
+				}
+				var frame = JSON.parse(this.inBuffer.slice(frameBegin, frameEnd));
+				this.inBuffer = this.inBuffer.slice(frameEnd) // remove frame from buffer
+				// frame consists of connection id, frame type, arbitrary other arguments
+				assert(frame instanceof Array && frame.length >= 2, 'Invalid frame');
+				var connectionId = frame.shift();
+				var frameType = frameTypes[frame.shift()];
+				assert(frameType, 'Unrecognized frame type');
+				var args = frame; args.unshift(connectionId); // put back connection ID
+				dispatchFrame[frameType].apply(this, args);
+			};
+		} catch (e) {
+			debug('PROTOCOL ERROR: ', (e.message || 'unknown error'));
+			this.shutdown(true);
+		};
+	};
+	this.shutdown = function (had_error) {
+		// close all outgoing TCP connections			 
+		for (connectionId in this.outConnections) {
+			if (had_error) {
+				this.closeOutgoing(connectionId, 'ProtocolError');
+			} else {
+				this.closeOutgoing(connectionId, 'UserConnectionReset');
+			}
+			var outConn = this.outConnections[connectionId]
+			outConn.listeners('connect') = [];
+			outConn.listeners('receive') = [];
+			outConn.listeners('eof') = [];
+			outConn.listeners('disconnect') = [];
+		};
+	};
+	this.send = function (frame) {
+		payload = JSON.stringify(frame);
+		this.inConnection.send(payload.length + ',' + payload, 'bytes');
+	};
+	this.closeOutgoing = function (connectionId, errorType) {
+		if (connectionId in this.outConnections) {
+			var code = errorCodes[errorType];
+			this.send([connectionId, FRAME_CLOSED, code]);
+			this.outConnections[connectionId].close();
+			delete this.outConnections[connectionId];
+		};
+	};
+	this.dispatchFrame = {
+		open: function (connectionId, host, port) {
+			assert(!(connectionId in this.outConnections), 'OPEN frame for existing connection');
+			assert(host && port, 'Invalid host or port');
+			var outConn = this.outConnections[connectionId] = node.tcp.createConnection(port, host);
+			outConn.setEncoding('bytes');
+			outConn
+				.addListener('connect', function () {
+					this.send([connectionId, FRAME_OPENED]);
+				})
+				.addListener('receive', function (data) {
+					this.send([connectionId, FRAME_DATA, data]);
+				})
+				.addListener('eof', function () {
+					this.closeOutgoing(connectionId, 'RemoteConnectionClosed');
+				})
+				.addListener('disconnect', function (had_error) {
+					this.closeOutgoing(connectionId, 'RemoteConnectionClosed');
+				});
+		},
+		close: function (connectionId) {
+			this.closeOutgoing(connectionId, 'UserConnectionReset');
+		},
+		data: function (connectionId, data) {
+			data = unescape(data)
+			this.outConnections[connectionId].send(data, 'bytes');
+		},
+	};	
+});
+
+msp.Proxy = Class(function () {
+	this.init = function () {
+		this.cspserver = csp.createServer(function(connection) {
+			proxyConnection = new msp.ProxyConnection(connection);
+		});
+	}
+	this.listen = function (port, host) {
+		this.cspserver.listen(port, host);
+	};
+});
+
+})(); // end closure w/ code for msp
 
 function start_server () {
-  var server = new msp.Proxy();
-  server.listen(8050);
-  puts("Proxying from http://:8050");
+	var server = new msp.Proxy();
+	server.listen(8050);
+	puts("Proxying from http://:8050");
 };
 start_server();
