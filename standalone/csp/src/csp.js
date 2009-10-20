cc896d60ed81c2025e237b8ad0a620545ea1ed18
Michael Carter <CarterMichael@gmail.com = Michael Carter CarterMichael@gmail.com>
2009-10-20 16:55:57 +0000

    Create basic structure for standalone protocol builds; Add standalone build of csp;

---
+++ standalone/csp/src/csp.js
@@ -0,0 +1,9 @@
+jsio('import jsio.csp.client as client');
+jsio('import jsio.csp.errors as errors');
+jsio('import jsio.csp.transports as transports');
+
+exports.CometSession = client.CometSession;
+exports.READYSTATE = client.READYSTATE;
+exports.errors = errors;
+exports.transports = transports;
+
