6ab7bf0c766dcbd4734a33eb593b59e8e26a9909
Martin Hunt <martin@mgh4.local>
2010-02-10 06:21:39 -0800

    update start_with_node to use relative paths

--- examples/world/start_with_node.js
+++ examples/world/start_with_node.js
@@ -1,6 +1,4 @@
 require('../../packages/jsio');
-jsio.path.__default__.unshift('packages');
-jsio.path.world = '.';
 
 jsio('import net');
 jsio('import logging');
@@ -8,9 +6,9 @@ jsio('import logging');
 //logging.getLogger('RTJPProtocol').setLevel(0);
 //logging.getLogger('node.csp.server').setLevel(0);
 //logging.getLogger('DelimitedProtocol').setLevel(0);
-logging.getLogger('world.server').setLevel(1);
+logging.getLogger('.world.server').setLevel(1);
 
-jsio("from world.server import WorldServer");
+jsio("from .world.server import WorldServer");
 w = new WorldServer();
 net.listen(w, 'csp', {port: 5555})
 
