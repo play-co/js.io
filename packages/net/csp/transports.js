db8ab0352b867b455c22e957f56af191c8def27f
Martin Hunt <martin@mgh4.local>
2010-02-16 00:47:00 -0800

    instanceof doesn't work for XMLHttpRequests in WebKit - fix with __proto__

--- packages/net/csp/transports.js
+++ packages/net/csp/transports.js
@@ -29,7 +29,8 @@ function canUseXHR(url) {
 	// if the URL requested is a different domain than the window,
 	// then we need to check for cross-domain support
 	if (window.XMLHttpRequest
-			&& xhr instanceof window.XMLHttpRequest
+			&& (xhr.__proto__ == XMLHttpRequest.prototype // WebKit Bug 25205
+				|| xhr instanceof window.XMLHttpRequest)
 			&& xhr.withCredentials !== undefined
 		|| window.XDomainRequest 
 			&& xhr instanceof window.XDomainRequest) {
