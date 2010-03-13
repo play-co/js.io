d2d683b3332de9f9af1f39cc205d82668985a457
Martin Hunt <martin@mgh4.local>
2010-03-13 01:48:59 -0800

    if you subscribe to the signal '__any' you'll get notified on every publish
    also, return this in publish and subscribe so that you can chain off these methods

--- packages/lib/PubSub.js
+++ packages/lib/PubSub.js
@@ -4,26 +4,28 @@ var ctx = jsio.__env.global;
 
 exports = Class(function() {
 	this.publish = function(signal) {
-		if(!this._subscribers) { return; }
+		if(this._subscribers) {
+			var args = Array.prototype.slice.call(arguments, 1);
+			if(this._subscribers.__any) {
+				var anyArgs = [signal].concat(args);
+				for(var i = 0, sub; sub = this._subscribers.__any[i]; ++i) {
+					sub.apply(ctx, args);
+				}
+			}
 		
-		var args = Array.prototype.slice.call(arguments, 1);
-		if(this._subscribers.__any) {
-			var anyArgs = [signal].concat(args);
-			for(var i = 0, sub; sub = this._subscribers.__any[i]; ++i) {
+			if(!this._subscribers[signal]) { return; }		
+			for(var i = 0, sub; sub = this._subscribers[signal][i]; ++i) {
 				sub.apply(ctx, args);
 			}
 		}
-		
-		if(!this._subscribers[signal]) { return; }		
-		for(var i = 0, sub; sub = this._subscribers[signal][i]; ++i) {
-			sub.apply(ctx, args);
-		}
+		return this;
 	}
 	
 	this.subscribe = function(signal) {
 		if(!this._subscribers) { this._subscribers = {}; }
 		if(!this._subscribers[signal]) { this._subscribers[signal] = []; }
 		this._subscribers[signal].push(bind.apply(ctx, Array.prototype.slice.call(arguments, 1)));
+		return this;
 	}
 });
 
