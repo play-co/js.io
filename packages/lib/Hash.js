333d6a7b78978fe0743960656e7a7a4546608b11
Martin Hunt <martin@mgh4.local>
2010-03-13 01:59:56 -0800

    add a basic Hash (Set) class doesn't yet have functionality for accessing the elements inside it or adding elements!

---
+++ packages/lib/Hash.js
@@ -0,0 +1,33 @@
+/**
+ * basic Hash/Set class
+ *  var h = new Hash('a', 'b', 'c');
+ *  h.contains('a') ==> true
+ *
+ *  var h = new Hash({a: '1', b: '2', c: '3'});
+ *  h.contains('1') ==> true
+ *  h.hasKey('a') ==> true
+ */
+exports = Class(function() {
+	this.init = function() {
+		this.m_keys = {};
+		this.m_dict = {};
+		this.m_values = {};
+		if (arguments.length == 1 && typeof arguments == 'object') {
+			var dict = arguments[0];
+			for (var i in dict) {
+				if (dict.hasOwnProperty(i)) {
+					this.m_keys[i] = true;
+					this.m_values[i] = dict[i];
+				}
+			}
+		} else {
+			for (var i = 0, len = arguments.length; i < len; i++) {
+				this.m_keys[arguments[i]] = true;
+				this.m_values[arguments[i]] = true;
+			};
+		}
+	}
+	
+	this.contains = function(val) { return this.m_values.hasOwnProperty(val); }
+	this.hasKey = this.containsKey = function(key) { return this.m_keys.hasOwnProperty(key); }
+});
