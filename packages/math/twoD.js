35b891252c0f5b4f5071af418eaeacb7a19800eb
Martin Hunt <martin@mgh4.local>
2010-09-10 19:53:31 -0700

    Refactor 2D point code into a Point class.
    Any method called on a Point instance will modify the instance.  Any method called directly on the Math2D object supports variable length objects and returns a new Point object.
    
    The following are equivalent:
    
    	import math.twoD as Math2D;
    
    	Math2D.translate(10, 10, -5, -2)
    	new Point(10, 10).translate(-5, -2)
    	new Point(10, 10).translate(new Point(-5, -2))
    	Math2D.translate(new Point(10, 10), -5, -2)
    	Math2D.translate(new Point(10, 10), new Point(-5, -2))
    
    However,
    
    	var pt = new Point(10, 10);
    	Math2D.translate(pt, -5, -2); -> returns new Point(5, 8);
    
    while:
    
    	var pt = new Point(10, 10);
    	pt.translate(-5, -2); -> modifies pt directly
    
    Also note that pt.translate(-5, -2); returns the point, so we can write:
    
    	var pt = new Point(10, 10).translate(-5, -2);

--- packages/math/twoD.js
+++ packages/math/twoD.js
@@ -1,60 +1,124 @@
-exports.add = exports.translate = function(pt, dx, dy) {
-	return typeof dx == 'number' ?
-		{
-			x: pt.x + dx,
-			y: pt.y + dy
+var Point = Class(function() {
+	this.init = function(x, y) {
+		if (typeof x == 'number') {
+			this.x = x;
+			this.y = y;
+		} else {
+			this.x = x.x;
+			this.y = x.y;
 		}
-		: {
-			x: pt.x + dx.x,
-			y: pt.y + dx.y
-		};
-}
-
-exports.subtract = function(pt, dx, dy) {
-	return typeof dx == 'number' ?
-		{
-			x: pt.x - dx,
-			y: pt.y - dy
-		}
-		: {
-			x: pt.x - dx.x,
-			y: pt.y - dx.y
-		};
-}
-
-exports.scale = function(s, pt) {
-	return {
-		x: pt.x * s,
-		y: pt.y * s
-	};
-}
-
-exports.addMagnitude = function(pt, m) {
-	return exports.setMagnitude(pt, exports.getMagnitude(pt) + m);
-}
-
-exports.setMagnitude = function(pt, m) {
-	var theta = Math.atan2(pt.y, pt.x);
-	return {
-		x: m * Math.cos(theta),
-		y: m * Math.sin(theta)
-	};
-}
-
-exports.getMagnitude = function(pt) {
-	return Math.sqrt(pt.x * pt.x + pt.y * pt.y);
-}
-
-exports.rotate = function(pt, r) {
-	var x = pt.x,
-		y = pt.y,
-		cosr = Math.cos(r),
-		sinr = Math.sin(r);
+	}
 	
-	return {
-		x: x * cosr - y * sinr,
-		y: x * sinr + y * cosr
-	};
+	this.rotate = function(r) {
+		var x = this.x,
+			y = this.y,
+			cosr = Math.cos(r),
+			sinr = Math.sin(r);
+		
+		this.x = x * cosr - y * sinr;
+		this.y = x * sinr + y * cosr;
+		
+		return this;
+	}
+	
+	this.add = function(x, y) {
+		if (typeof x == 'number') {
+			this.x += x;
+			this.y += y;
+		} else {
+			this.x += x.x;
+			this.y += x.y;
+		}
+		return this;
+	}
+	
+	this.subtract = function(x, y) {
+		if (typeof x == 'number') {
+			this.x -= x;
+			this.y -= y;
+		} else {
+			this.x -= x.x;
+			this.y -= x.y;
+		}
+		return this;
+	}
+
+	this.scale = function(s) {
+		this.x *= s;
+		this.y *= s;
+		return this;
+	}
+	
+	this.setMagnitude = function(m) {
+		var theta = Math.atan2(this.y, this.x);
+		this.x = m * Math.cos(theta);
+		this.y = m * Math.sin(theta);
+		return this;
+	}
+	
+	this.addMagnitude = function(m) { return this.setMagnitude(this.getMagnitude() + m); }
+	this.getMagnitude = function() { return Math.sqrt(this.x * this.x + this.y * this.y); }
+	
+	this.inBox = function(x1, y1, x2, y2) {
+		var x = this.x,
+			y = this.y;
+		return x >= x1 && x <= x2 && y >= y1 && y <= y2;
+	}
+});
+
+exports.Point = Point;
+
+exports.add = exports.translate = function(a, b, c, d) {
+	switch(arguments.length) {
+		case 2: return new Point(a).add(b);
+		case 3: return new Point(a).add(b, c);
+		case 4: return new Point(a, b).add(c, d);
+	}
 }
 
-exports.inBox = function(pt, x1, y1, x2, y2) { return pt.x >= x1 && pt.x <= x2 && pt.y >= y1 && pt.y <= y2; }
+exports.subtract = function(a, b, c, d) {
+	switch(arguments.length) {
+		case 2: return new Point(a).subtract(b);
+		case 3: return new Point(a).subtract(b, c);
+		case 4: return new Point(a, b).subtract(c, d);
+	}
+}
+
+exports.scale = function(a, b, c) {
+	switch(arguments.length) {
+		case 2: return new Point(a).scale(b);
+		case 3: return new Point(a, b).scale(c);
+	}
+}
+
+exports.setMagnitude = function(a, b, c) {
+	switch(arguments.length) {
+		case 2: return new Point(a).setMagnitude(c);
+		case 3: return new Point(a, b).setMagnitude(c);
+	}
+}
+
+exports.addMagnitude = function(a, b, c) {
+	switch(arguments.length) {
+		case 2: pt = new Point(a); break;
+		case 3: pt = new Point(a, b); b = c; break;
+	}
+	
+	return pt.addMagnitude(b);
+}
+
+exports.getMagnitude = function(a, b) { return new Point(a, b).getMagnitude(); }
+
+exports.rotate = function(a, b, c) {
+	switch(arguments.length) {
+		case 2: return new Point(a).rotate(b);
+		case 3: return new Point(a, b).rotate(c);
+	}
+}
+
+exports.inBox = function(a, b, c, d, e, f) {
+	switch(arguments.length) {
+		case 5: return new Point(a).inBox(b, c, d, e);
+		case 6: return new Point(a, b).inBox(c, d, e, f);
+	}
+}
