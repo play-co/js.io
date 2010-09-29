ec2f5cf1e02144013c78a0c161a4f253ff13db05
Martin Hunt <mghunt@gmail.com>
2010-09-28 19:09:47 -0700

    make node_run callable from anywhere by modifying jsio paths temporarily

--- compilers/jsio_compile/node_run.js
+++ compilers/jsio_compile/node_run.js
@@ -1,6 +1,15 @@
 var sys = require('sys'),
 	jsioPath = (process.argv[2] ? process.argv[2] : './jsio');
 
+process.argv.splice(2, 1);
+
 require(jsioPath + '/jsio');
 
-jsio('import .compiler').start();
+var pathStat = jsio.__util.splitPath(__filename),
+	path = jsio.__util.makeRelativePath(pathStat.directory, process.cwd());
+
+jsio.addPath(path);
+var compiler = jsio('import compiler');
+jsio.removePath(path);
+
+compiler.start();
