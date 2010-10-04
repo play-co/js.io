a70dd77a9551fb4130181b69d7e9db9e976096c8
Martin Hunt <mghunt@gmail.com>
2010-10-04 16:15:33 -0700

    update Sortable to take multiple sort indexers passed in to sort(), independently handled with results joined by '|' characters

--- packages/lib/Sortable.js
+++ packages/lib/Sortable.js
@@ -36,54 +36,82 @@ function ensurePadding(n) {
 exports.sort = function(arr, indexer) {
 	
 	var len = arr.length,
-		index = new Array(len);
+		index = new Array(len),
+		result = new Array(len),
+		toString = new Array(len),
+		indexers = Array.prototype.slice.call(arguments, 1),
+		haveMultiple = !!indexers[1];
 	
 	function sortIndex(i) { return this[i]; }
 	
-	for (var i = 0; i < len; ++i) {
-		index[i] = indexer.call(arr[i], i);
-		arr[i].toStringPush(bind(index, sortIndex, i));
+	if (haveMultiple) {
+		for (var i = 0; i < len; ++i) {
+			result[i] = [];
+		}
 	}
 	
-	if (typeof index[0] == 'number') {
-		// we do two passes here:
-		//  1: find the max and min numerical indices
-		//  2: convert the indices to strings with appropriate zero-padding
-		var largest = index[0],
-			smallest = index[0];
-		
-		for (var i = 1; i < len; ++i) {
-			if (index[i] > largest) {
-				largest = index[i];
-			} else if (index[i] < smallest) {
-				smallest = index[i];
-			}
-		}
-		
-		// we have to be very careful here - large floating point numbers will break the
-		// string padding code
-		var paddingPositive = String(Math.floor(largest)).length,
-			paddingNegative = String(Math.floor(smallest)).length;
-		
-		ensurePadding(Math.max(paddingPositive, paddingNegative));
-		
-		var strLen;
+	for (var k = 0, indexer; indexer = indexers[k]; ++k) {
 		for (var i = 0; i < len; ++i) {
-			var val = index[i];
-			if (val < 0) {
-				val = -(smallest - val);
-				strLen = ('' + Math.floor(val)).length;
-				index[i] = '-' + zeroPadding[paddingNegative - strLen] + val;
-			} else {
-				strLen = ('' + Math.floor(val)).length;
-				index[i] = zeroPadding[paddingPositive - strLen] + val;
+			index[i] = indexer.call(arr[i], i);
+		}
+		
+		if (typeof index[0] == 'number') {
+			// we do two passes here:
+			//  1: find the max and min numerical indices
+			//  2: convert the indices to strings with appropriate zero-padding
+			var largest = index[0],
+				smallest = index[0];
+
+			for (var i = 1; i < len; ++i) {
+				if (index[i] > largest) {
+					largest = index[i];
+				} else if (index[i] < smallest) {
+					smallest = index[i];
+				}
+			}
+
+			// we have to be very careful here - large floating point numbers will break the
+			// string padding code
+			var paddingPositive = String(Math.floor(largest)).length,
+				paddingNegative = String(Math.floor(smallest)).length;
+
+			ensurePadding(Math.max(paddingPositive, paddingNegative));
+
+			var strLen;
+			for (var i = 0; i < len; ++i) {
+				var val = index[i];
+				if (val < 0) {
+					val = -(smallest - val);
+					strLen = ('' + Math.floor(val)).length;
+					index[i] = '-' + zeroPadding[paddingNegative - strLen] + val;
+				} else {
+					strLen = ('' + Math.floor(val)).length;
+					index[i] = zeroPadding[paddingPositive - strLen] + val;
+				}
 			}
 		}
+		
+		if (haveMultiple) {
+			for (var i = 0; i < len; ++i) {
+				result[i].push(index[i]);
+			}
+		} else {
+			result = index;
+		}
+	}
+	
+	for (var i = 0; i < len; ++i) {
+		if (haveMultiple) {
+			result[i] = result[i].join('|');
+		}
+		
+		toString[i] = arr[i].toString;
+		arr[i].toString = bind(result, sortIndex, i);
 	}
 	
 	Array.prototype.sort.apply(arr);
 	
 	for (var i = 0; i < len; ++i) {
-		arr[i].toStringPop(indexer);
+		arr[i].toString = toString[i];
 	}
 }
