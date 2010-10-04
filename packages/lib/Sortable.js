f6c233c6114d2a431e648d7e32d7a7e65930e474
Martin Hunt <mghunt@gmail.com>
2010-10-04 15:49:34 -0700

    lib.Sortable now support indexers that return integers or floating points (up to a limit) for quickly sorting arrays using toString rather than array.sort(func)

--- packages/lib/Sortable.js
+++ packages/lib/Sortable.js
@@ -5,7 +5,7 @@ import .LogClass;
 exports = LogClass('lib.Sortable', function(logger) {
 	
 	this.toStringPush = function(indexer) {
-		if (!this._toString) {
+		if (!this._toString || !this._toString.push) {
 			this._toString = [this.toString];
 		} else {
 			this._toString.push(this.toString);
@@ -19,14 +19,69 @@ exports = LogClass('lib.Sortable', function(logger) {
 	}
 });
 
+var zeroPadding = [];
+function ensurePadding(n) {
+	for (var i = zeroPadding.length; i < n; ++i) {
+		var str = [];
+		for (var j = 0; j < i; ++j) { str.push('0'); }
+		zeroPadding[i] = str.join('');
+	}
+}
+
+/**
+ * Here we handle arbitrary sorting indexes transparently converting numbers to strings
+ * for efficient sorting with toString.  Unfortunately, this does not work for large floating
+ * point values, but that functionality could theoretically be added if desired.
+ */
 exports.sort = function(arr, indexer) {
 	
-	var len = arr.length;
+	var len = arr.length,
+		index = new Array(len);
+	
+	function sortIndex(i) { return this[i]; }
+	
 	for (var i = 0; i < len; ++i) {
-		arr[i].toStringPush(indexer);
+		index[i] = indexer.call(arr[i], i);
+		arr[i].toStringPush(bind(index, sortIndex, i));
 	}
 	
-	arr.sort();
+	if (typeof index[0] == 'number') {
+		// we do two passes here:
+		//  1: find the max and min numerical indices
+		//  2: convert the indices to strings with appropriate zero-padding
+		var largest = index[0],
+			smallest = index[0];
+		
+		for (var i = 1; i < len; ++i) {
+			if (index[i] > largest) {
+				largest = index[i];
+			} else if (index[i] < smallest) {
+				smallest = index[i];
+			}
+		}
+		
+		// we have to be very careful here - large floating point numbers will break the
+		// string padding code
+		var paddingPositive = String(Math.floor(largest)).length,
+			paddingNegative = String(Math.floor(smallest)).length;
+		
+		ensurePadding(Math.max(paddingPositive, paddingNegative));
+		
+		var strLen;
+		for (var i = 0; i < len; ++i) {
+			var val = index[i];
+			if (val < 0) {
+				val = -(smallest - val);
+				strLen = ('' + Math.floor(val)).length;
+				index[i] = '-' + zeroPadding[paddingNegative - strLen] + val;
+			} else {
+				strLen = ('' + Math.floor(val)).length;
+				index[i] = zeroPadding[paddingPositive - strLen] + val;
+			}
+		}
+	}
+	
+	Array.prototype.sort.apply(arr);
 	
 	for (var i = 0; i < len; ++i) {
 		arr[i].toStringPop(indexer);
