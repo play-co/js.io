169d02489ae52e02fa489dd1a974142407e24af7
Martin Hunt <mghunt@gmail.com>
2010-12-18 00:04:19 -0800

    use multiple strings to avoid bugs with jsio compiler until we add better parsing

--- packages/preprocessors/import.js
+++ packages/preprocessors/import.js
@@ -1,5 +1,5 @@
 var importExpr = /^(\s*)(import\s+.*|from\s+.*)$/gm;
 
 exports = function(path, moduleDef, opts) {
-	moduleDef.src = moduleDef.src.replace(importExpr, '$1jsio("$2");');
+	moduleDef.src = moduleDef.src.replace(importExpr, '$1' + 'jsio' + '("$2");');
 }
