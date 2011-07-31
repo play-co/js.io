e5c38babe7d1e6c478b5b8a28c7904b658ae273c
Martin Hunt <mghunt@gmail.com>
2011-07-31 15:57:12 -0700

    cache and compress jsio_compile code

--- compilers/jsio_compile/build.js
+++ compilers/jsio_compile/build.js
@@ -2,11 +2,16 @@
 
 // run "node build.js" to generate a self-contained jsio_compile script
 
-var TARGET = 'build/jsio_compile';
-
 var sys = require('sys'),
 	fs = require('fs'),
+	path = require('path'),
 	node = process.argv[0];
+
+var exec = require('child_process').exec;
+
+var BUILD_DIR = 'build';
+var TARGET = path.join(BUILD_DIR, 'jsio_compile');
+var CACHE_PATH = path.join(BUILD_DIR, '.cache');
 
 require('./jsio/jsio');
 
@@ -15,64 +20,61 @@ jsio('from base import *');
 logger = logging.get('compiler');
 logging.get('preprocessors.compiler').setLevel(0);
 
-// get access to jsio path util functions
-jsio('import util.path');
+jsio('import lib.Callback');
+var cb = new lib.Callback();
 
-try {
-	var buildDir = fs.statSync('build');
-} catch(e) {
-	// seems like errno 2 is a 'does not exist' error
-	if (e.errno != 2) { throw e; }
-}
+exec('mkdir -p ' + BUILD_DIR, null, cb.chain());
+exec('mkdir -p ' + CACHE_PATH, null, cb.chain());
 
-if (!buildDir || !buildDir.isDirectory()) {
-	fs.mkdirSync('build', 0777);
-}
+cb.run(doCompile);
 
-var compiler = jsio('import preprocessors.compiler');
+function doCompile() {
+	// get access to jsio path util functions
+	jsio('import util.path');
 
-var interface = jsio('import .node_interface');
-compiler.setCompressor(interface.compressor);
+	var compiler = jsio('import preprocessors.compiler');
 
-compiler.compile('import preprocessors.compiler');
-compiler.compile('import .compiler', {
-	dynamicImports: {
-		COMPILER: 'import .node_interface'
-	}
-});
+	var interface = jsio('import .node_interface');
+	compiler.setCompressor(interface.compressor);
 
-// grab a full copy of jsio
-walk('jsio', function(path, files, dirs) {
-	files.forEach(function(file) {
-		if (/\.js$/.test(file)) {
-			var module = file.replace(/^jsio\//, '').replace(/\.js$/, '').replace(/\//g, '.');
-			if (module != 'jsio') {
-				compiler.compile('import ' + module);
-			}
+	compiler.compile('import preprocessors.compiler');
+	compiler.compile('import .compiler', {
+		dynamicImports: {
+			COMPILER: 'import .node_interface'
 		}
 	});
-});
 
-var exec = require('child_process').exec;
-
-exec("which " + node, function(error, stdout, stderr) {
-	var nodeLocation = stdout.replace(/\n/g, '');
-	logger.info('Found node at', nodeLocation)
-	exec(nodeLocation + " --version", function(error, stdout, stderr) {
-		var nodeVersion = stdout.replace(/\n/g, '');;
-		logger.info('Using node version', nodeVersion);
-		compiler.generateSrc({compressResult: true, preserveJsioSource: true}, function(src) {
-			var fd = fs.openSync(TARGET, 'w');
-			fs.writeSync(fd, '#!' + nodeLocation + '\n');
-			fs.writeSync(fd, src);
-			fs.writeSync(fd, 'jsio("import .compiler").start()');
-			fs.closeSync(fd);
-			
-			exec("chmod +x " + TARGET);
-			logger.info('Wrote', TARGET);
+	// grab a full copy of jsio
+	walk('jsio', function(path, files, dirs) {
+		files.forEach(function(file) {
+			if (/\.js$/.test(file)) {
+				var module = file.replace(/^jsio\//, '').replace(/\.js$/, '').replace(/\//g, '.');
+				if (module != 'jsio') {
+					compiler.compile('import ' + module);
+				}
+			}
 		});
 	});
-});
+
+	exec("which " + node, function(error, stdout, stderr) {
+		var nodeLocation = stdout.replace(/\n/g, '');
+		logger.info('Found node at', nodeLocation)
+		exec(nodeLocation + " --version", function(error, stdout, stderr) {
+			var nodeVersion = stdout.replace(/\n/g, '');;
+			logger.info('Using node version', nodeVersion);
+			compiler.generateSrc({compressorCachePath: CACHE_PATH, compressSources: true, compressResult: true, preserveJsioSource: true}, function(src) {
+				var fd = fs.openSync(TARGET, 'w');
+				fs.writeSync(fd, '#!' + nodeLocation + '\n');
+				fs.writeSync(fd, src);
+				fs.writeSync(fd, 'jsio("import .compiler").start()');
+				fs.closeSync(fd);
+
+				exec("chmod +x " + TARGET);
+				logger.info('Wrote', TARGET);
+			});
+		});
+	});
+}
 
 // ********
 // node-specific walk implementation
