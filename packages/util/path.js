0a1609d6763717d71a245aeb6600218493de8172
Martin Hunt <mghunt@gmail.com>
2011-08-29 17:43:26 -0700

    add splitExt and rename buildPath to join

--- packages/util/path.js
+++ packages/util/path.js
@@ -1,6 +1,18 @@
 var util = jsio.__jsio.__util;
 
-exports.buildPath = util.buildPath;
+exports.join = util.buildPath;
 exports.resolveRelativePath = util.resolveRelativePath;
 exports.splitPath = util.splitPath;
 exports.makeRelativePath = util.makeRelativePath;
+exports.splitExt = function(path) {
+	var res = exports.splitPath(path);
+	var i = res.filename.lastIndexOf('.');
+	if (i == -1) {
+		res.basename = res.filename;
+		res.ext = '';
+	} else {
+		res.basename = res.filename.substring(0, i);
+		res.ext = res.filename.substring(i);
+	}
+	return res;
+}
