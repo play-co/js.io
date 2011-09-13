42dedc80b1efb83d0ec37e46fd7f7cffa8a13d22
Martin Hunt <mghunt@gmail.com>
2011-09-13 14:59:50 -0700

    if a js.io line is commented out with //, ignore it!

--- packages/preprocessors/import.js
+++ packages/preprocessors/import.js
@@ -1,5 +1,12 @@
 var importExpr = /^(\s*)(import\s+.*|from\s+.*)$/gm;
 
+function replace(match) {
+	if (!/\/\//.test(match[1])) {
+		return match[1] + 'jsio' + match[2]
+	}
+	return match[0];
+}
+
 exports = function(path, moduleDef, opts) {
 	moduleDef.src = moduleDef.src.replace(importExpr, '$1' + 'jsio' + '("$2");');
 }
