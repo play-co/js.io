87f40bca3c1b90d002c5dc7c1297d2ca36adfe12
Billy Lamberta <b@lamberta.org>
2012-04-10 17:48:39 -0700

    Added basic in-file documentation to base.js. Changed the order of some sections to keep properties and methods grouped together.

--- packages/base.js
+++ packages/base.js
@@ -1,9 +1,22 @@
-exports.log = jsio.__env.log;
-exports.GLOBAL = jsio.__env.global;
+/**
+ * js.io base functions.
+ * A collection of utility functions added to the global scope.
+ * @namespace
+ */
 
 var SLICE = Array.prototype.slice;
 
-/* Use native isArray if available
+/**
+ * The global object.
+ * @property
+ */
+exports.GLOBAL = jsio.__env.global;
+
+
+/**
+ * Returns true is an object is an array, false if it is not. Use native isArray if available.
+ * @param {*} obj
+ * @return {boolean}
  */
 if (typeof Array.isArray === 'function') {
 	exports.isArray = Array.isArray;
@@ -13,6 +26,15 @@ if (typeof Array.isArray === 'function') {
 	}
 }
 
+/**
+ * Creates a new function that, when called, calls the given function with the context provided as the 'this' value.
+ * !!perhaps there is a way to conform the native Function.prototype.bind to this for performnce?
+ *
+ * @param {} context The object to specify as 'this' in the bound function.
+ * @param {function} method Function to bind to a new context.
+ * @param {... *} VARGS Arguments to apply to the given function.
+ @ return {function}
+ */
 exports.bind = function(context, method /*, VARGS*/) {
 	if(arguments.length > 2) {
 		var args = SLICE.call(arguments, 2);
@@ -38,6 +60,18 @@ exports.bind = function(context, method /*, VARGS*/) {
 	}
 }
 
+/**
+ * Creates a new constructor function, and make it a subclass of the parent if provided.
+ * @param {string=} name Optionally assign the new constructor function a name, assigned as an object property.
+ * @param {function=} parent The class to inherit from. If omitted, then create a new class with no superclass.
+ * @param {function} proto
+ * @return {constructor}
+ *
+ * The returned construtor contains the following prototype properties:
+ * @property {function} constructor
+ * @property {Class} __parentClass__
+ * @property {string} __class__
+ */
 exports.Class = function(parent, proto) {
 	if (typeof parent == 'string') {
 		var name = arguments[0],
@@ -70,14 +104,33 @@ exports.Class = function(parent, proto) {
 			if (!f) { throw new Error('method ' + method + ' does not exist'); }
 			return f.apply(context, args || []);
 		} : null;
-	
+
+	/**
+	 * Instantiate the proto function and assign it as the prototype of the new class object.
+	 * @property {object} 
+	 */
 	cls.prototype = new proto(logger || supr, logger && supr);
 	cls.prototype.constructor = cls;
+
+	/**
+	 * @private
+	 * @property
+	 */
 	cls.prototype.__parentClass__ = parent;
+
+	/**
+	 * Name of class, set as optional first argument to Class. If none given, this is undefined.
+	 * @private
+	 * @property {string|undefined}
+	 */
 	if (name) { cls.prototype.__class__ = name; }
+	
 	return cls;
 }
 
+/**
+ * @extends {Error}
+ */
 var ErrorParentClass = exports.Class(Error, function() {
 	this.init = function() {
 		var err = Error.prototype.constructor.apply(this, arguments);
@@ -89,34 +142,14 @@ var ErrorParentClass = exports.Class(Error, function() {
 	}
 });
 
-exports.Class.defaults = 
-exports.merge = function(base, extra) {
-	base = base || {};
-	
-	for (var i = 1, len = arguments.length; i < len; ++i) {
-		var copyFrom = arguments[i];
-		for (var key in copyFrom) {
-			if (copyFrom.hasOwnProperty(key) && !base.hasOwnProperty(key)) {
-				base[key] = copyFrom[key];
-			}
-		}
-	}
-	
-	return base;
-}
-
-exports.delay = function(orig, timeout) {
-	var _timer = null;
-	var ctx, args;
-	var f = function() { orig.apply(ctx, args); }
-	return function() {
-		ctx = this;
-		args = arguments;
-		if (_timer) { clearTimeout(_timer); }
-		_timer = setTimeout(f, timeout || 0);
-	}
-}
-
+/**
+ * Used to be used to initialize classes and call init, but this tends to be done explictly.
+ * @deprecated
+ * @param {} proto
+ * @param {} supr
+ * @param {} defaults
+ * @param {} post
+ */
 exports.Class.ctor = function(proto, supr, defaults, post) {
 	if (!supr) {
 		supr = function(ctx, method, args) {
@@ -136,7 +169,58 @@ exports.Class.ctor = function(proto, supr, defaults, post) {
 	}
 }
 
-// keep logging local variables out of other closures in this file!
+/**
+ * Combine the properties of multiple objects.
+ * @param {object} base
+ * @param {object} extra
+ * @param {... object} VARGS
+ * @rerturn {object} Returns the first object argument, with the properties of subsequent objects added.
+ */
+exports.merge = exports.Class.defaults = function(base, extra /*, VARGS*/) {
+	base = base || {};
+	
+	for (var i = 1, len = arguments.length; i < len; ++i) {
+		var copyFrom = arguments[i];
+		for (var key in copyFrom) {
+			if (copyFrom.hasOwnProperty(key) && !base.hasOwnProperty(key)) {
+				base[key] = copyFrom[key];
+			}
+		}
+	}
+	
+	return base;
+}
+
+/**
+ * Returns a function that, when called, executes the original function at the given delay.
+ * @param {function} orig Function to execute after a specified delay
+ * @param {number} timeout Milliseconds to delay function execution
+ * @return {function}
+ */
+exports.delay = function(orig, timeout) {
+	var _timer = null;
+	var ctx, args;
+	var f = function() { orig.apply(ctx, args); }
+	return function() {
+		ctx = this;
+		args = arguments;
+		if (_timer) { clearTimeout(_timer); }
+		_timer = setTimeout(f, timeout || 0);
+	}
+}
+
+/**
+ * Print arguments to screen
+ * @params {... *} VARGS Arguments to log to the console for inspection.
+ * @return {string} Arguments concatentated together and return as a string.
+ */
+exports.log = jsio.__env.log;
+
+/**
+ * Keep logging local variables out of other closures in this file.
+ * !!Maybe this should be in a separate file.
+ * @namespace
+ */
 exports.logging = (function() {
 	
 	// logging namespace, this is what is exported
@@ -149,27 +233,71 @@ exports.logging = (function() {
 		},
 		loggers = {}, // effectively globals - all loggers and a global production state
 		production = false;
+
 	var gPrefix = '';
+
+	/**
+	 * @param {string} prefix
+	 */
 	logging.setPrefix = function(prefix) { gPrefix = prefix + ' '; }
+
+	/**
+	 * @param {boolean} prod
+	 */
 	logging.setProduction = function(prod) { production = !!prod; }
+
+	/**
+	 * !!Should this really create a new logger if lookup fails?
+	 * @param {} name
+	 * @return {Logger}
+	 */
 	logging.get = function(name) {
 		return loggers.hasOwnProperty(name) ? loggers[name]
 			: (loggers[name] = new Logger(name));
-	}
+	};
+
+	/**
+	 * @param {string} name
+	 * @param {} _logger
+	 */
 	logging.set = function(name, _logger) {
 		loggers[name] = _logger;
-	}
-	
+	};
+
+	/**
+	 * @return {object} Collection of all loggers.
+	 */
 	logging.getAll = function() { return loggers; }
 
+	/**
+	 * @private
+	 * @param {} pkg
+	 * @param {} ctx
+	 */
 	logging.__create = function(pkg, ctx) { ctx.logger = logging.get(pkg); }
-	
+
+	/**
+	 * @constructor
+	 */
 	var Logger = exports.Class(function() {
 		this.init = function(name, level) {
+
+			/**
+			 * @private
+			 * @property {string}
+			 */
 			this._name = name;
+
+			/**
+			 * @private
+			 * @property {number}
+			 */
 			this._level = level || logging.LOG;
 		}
-		
+
+		/**
+		 * @param {number}
+		 */
 		this.setLevel = function(level) { this._level = level; }
 	
 		function makeLogFunction(level, type) {
@@ -183,12 +311,35 @@ exports.logging = (function() {
 				return arguments[0];
 			}
 		}
-	
+
+		/**
+		 *
+		 */
 		this.setListener = function(listener) { this._listener = listener; }
+
+		/**
+		 *
+		 */
 		this.debug = makeLogFunction(logging.DEBUG, "DEBUG");
+
+		/**
+		 *
+		 */
 		this.log = makeLogFunction(logging.LOG, "LOG");
+
+		/**
+		 *
+		 */
 		this.info = makeLogFunction(logging.INFO, "INFO");
+
+		/**
+		 *
+		 */
 		this.warn = makeLogFunction(logging.WARN, "WARN");
+
+		/**
+		 *
+		 */
 		this.error = makeLogFunction(logging.ERROR, "ERROR");
 	});
 
