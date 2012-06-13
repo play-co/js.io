62c69647fcd8f93abbeee8515be2e6f09950c4b2
Martin Hunt <mghunt@gmail.com>
2012-06-13 12:04:31 -0700

    remove debug code

--- packages/preprocessors/cls.js
+++ packages/preprocessors/cls.js
@@ -5,5 +5,5 @@ exports = function(path, moduleDef, opts) {
 	var moduleCtor = moduleDef.path.replace(/(^[.\/]+|\.([^.]+?)$)/g, '').replace(/[\/.]/g, '_');
 	moduleDef.src = moduleDef.src
 		.replace(class2Export, 'var $1=Class;$1=$1(')
-		.replace(classExport, 'var ' + moduleCtor + '=Class;$1exports = foo = ' + moduleCtor + '(')
+		.replace(classExport, 'var ' + moduleCtor + '=Class;$1exports=' + moduleCtor + '(')
 }
