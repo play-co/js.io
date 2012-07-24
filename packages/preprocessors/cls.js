5ad0312d748320823ab1d21f8414eab6407738cd
Martin Hunt <mghunt@gmail.com>
2012-07-23 19:36:26 -0700

    ... broke the CPU profiler.  Add both hacks together.

--- packages/preprocessors/cls.js
+++ packages/preprocessors/cls.js
@@ -1,13 +1,20 @@
-var classExport = /^(.*?)exports\s*=\s*Class\(/gm;
-var class2Export = /(^|\s+)([a-zA-Z0-9\.$]+)\s*=\s*Class\(/gm;
+
+// var F = exports = Class
+// exports = Class
+var classExport = /^(.*?)exports[ \t]*=[ \t]*Class\(/gm;
+
+// var F = Class
+// exports.F = Class
+// var F = exports.F = Class
+var class2Export = /^(.*?[ \t]+)?([a-zA-Z0-9\.$]+)[ \t]*=[ \t]*Class\(/gm;
 
 function replacer(base, prefix, name) {
-	return prefix + name + '=__class__(function ' + name.replace(/\./g, '_') + '(){return this.init&&this.init.apply(this,arguments)},';
+	return name + '=__class__;' + (prefix || '') + name + '=' + name + '(function ' + name.replace(/[\.]/g, '_') + '(){return this.init&&this.init.apply(this,arguments)},';
 }
 
 exports = function(path, moduleDef, opts) {
-	var moduleCtor = moduleDef.path.replace(/(^[.\/]+|\.([^.]+?)$)/g, '').replace(/[\/.]/g, '_');
+	var moduleCtor = moduleDef.path.replace(/(^[.\/]+|\.([^.]+?)$)/g, '').replace(/[\/\-\.]/g, '_');
 	moduleDef.src = moduleDef.src
-		.replace(classExport, '$1exports=__class__(function ' + moduleCtor + '(){return this.init&&this.init.apply(this,arguments)},')
+		.replace(classExport, 'var ' + moduleCtor + '=__class__;$1exports=' + moduleCtor + '(function ' + moduleCtor + '(){return this.init&&this.init.apply(this,arguments)},')
 		.replace(class2Export, replacer);
 }
