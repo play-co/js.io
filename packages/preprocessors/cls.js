0ad045e07e13cf988b9b41df4ad75359e2fe1b92
Martin Hunt <mghunt@gmail.com>
2012-07-23 18:42:13 -0700

    refactor Class into two pieces and inline constructor functions so that the Chrome heap profiler can identify class names

--- packages/preprocessors/cls.js
+++ packages/preprocessors/cls.js
@@ -1,9 +1,13 @@
 var classExport = /^(.*?)exports\s*=\s*Class\(/gm;
-var class2Export = /var\s*([a-zA-Z0-9$]+)\s*=\s*Class\(/;
+var class2Export = /(^|\s+)([a-zA-Z0-9\.$]+)\s*=\s*Class\(/gm;
+
+function replacer(base, prefix, name) {
+	return prefix + name + '=__class__(function ' + name.replace(/\./g, '_') + '(){return this.init&&this.init.apply(this,arguments)},';
+}
 
 exports = function(path, moduleDef, opts) {
 	var moduleCtor = moduleDef.path.replace(/(^[.\/]+|\.([^.]+?)$)/g, '').replace(/[\/.]/g, '_');
 	moduleDef.src = moduleDef.src
-		.replace(class2Export, 'var $1=Class;$1=$1(')
-		.replace(classExport, 'var ' + moduleCtor + '=Class;$1exports=' + moduleCtor + '(')
+		.replace(classExport, '$1exports=__class__(function ' + moduleCtor + '(){return this.init&&this.init.apply(this,arguments)},')
+		.replace(class2Export, replacer);
 }
