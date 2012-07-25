1d4266d915abb54ab0013398f54ca1dd944ab15e
Billy Lamberta <b@lamberta.org>
2012-07-25 16:17:49 -0700

    Copied over math.util from our older timestep repo. Contains extra comments and removes 'rand'.

--- packages/math/util.js
+++ packages/math/util.js
@@ -1,23 +1,47 @@
 jsio('import lib.Enum as Enum');
 
 /**
- * @namespace
+ * @package math.util;
+ */
+
+/**
+ * Interpolate between values a and b at the point x in the interval.
  */
 
 exports.interpolate = function(a, b, x) { return a * (1 - x) + b * x; }
 
+/**
+ * Return a random integer between a and b. Optionally, a random seed can be
+ * given.
+ */
+
 exports.random = function(a, b, rand) { return a + ((rand || Math.random)() * (b - a) | 0); }
 
+/**
+ * Return a random integer between a and b, inclusive. Optionally, a random seed
+ * can be given.
+ */
+
 exports.randomInclusive = function(a, b, rand) { return exports.random(a, b+1, rand); }
-exports.rand = Math.random;
-//FIXME integer is a reserved word XXX
-exports.integer = exports.truncate = function(a) { return a | 0; }
+
+/**
+ * Return a value where min <= num <= max.
+ */
 
 exports.clip = function(num, min, max) { return Math.max(Math.min(num, max), min); }
+
+/**
+ * Return the sign of a number.
+ */
 
 exports.sign = function (num) {
 	return num && num / Math.abs(num);
 };
+
+/**
+ * Rounding a value with the given precision, given the provided rounding
+ * method.
+ */
 
 var round = exports.round = function(a, precision, method) {
 	if (!method || method == round.ROUND_HALF_AWAY_FROM_ZERO) {
@@ -52,6 +76,14 @@ var round = exports.round = function(a, precision, method) {
 	return (integer + round(frac * p, 0, method) / p).toFixed(precision);
 }
 
-round.alt = true;
+/**
+ * Available rounding possibilities.
+ */
 
-Enum.call(round, 'ROUND_HALF_UP', 'ROUND_HALF_AWAY_FROM_ZERO', 'ROUND_HALF_TO_EVEN', 'ROUND_HALF_STOCHASTIC', 'ROUND_HALF_ALTERNATE');
+Enum.call(round, 'ROUND_HALF_UP', 'ROUND_HALF_AWAY_FROM_ZERO', 'ROUND_HALF_TO_EVEN', 'ROUND_HALF_TO_ODD', 'ROUND_HALF_STOCHASTIC', 'ROUND_HALF_ALTERNATE');
+
+/**
+ * Alternating flag for rounding strategy ROUND_HALF_ALTERNATE.
+ */
+
+round.alt = true;
