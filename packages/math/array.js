df1605d9d1a6dcab3c68bcb992bbc821009693fb
Billy Lamberta <b@lamberta.org>
2012-07-25 16:28:12 -0700

    Moved math.array over from our timestep/math/array.

--- packages/math/array.js
+++ packages/math/array.js
@@ -1,8 +1,14 @@
-jsio('import .util');
+jsio('import math.util as util');
 
 /**
- * @namespace
+ * @package math.array;
+ *
+ * Functions to manipulate one or more arrays in tandem.
  */
+
+/**
+ * Returns the weighted average
+ */ 
 
 exports.weightedAverage = function (a, w, n) {
 	n = n || a.length;
@@ -13,14 +19,22 @@ exports.weightedAverage = function (a, w, n) {
 	return s / n;
 }
 
-exports.subtract = function(a, b) {
+/**
+ * Subtract two arrays, (a - b)
+ */
+
+exports.subtract = function (a, b) {
 	var length = a.length,
-		diff = new Array(length);
+			diff = new Array(length);
 	for (var i = 0; i < length; ++i) {
-		diff[i] = b[i] - a[i];
+		diff[i] = a[i] - b[i];
 	}
 	return diff;
-}
+};
+
+/**
+ * Average an array.
+ */
 
 exports.average = function (a, n) {
 	n = n || a.length;
@@ -30,6 +44,10 @@ exports.average = function (a, n) {
 	}
 	return s / n;
 }
+
+/**
+ * Return the standard deviation of an array.
+ */
 
 exports.stddev = function (a, n) {
 	var avg = exports.average(a, n);
@@ -42,6 +60,10 @@ exports.stddev = function (a, n) {
 	return Math.sqrt(s / (n - 1));
 }
 
+/**
+ * Shuffle an array. Takes an optional random seed.
+ */
+
 exports.shuffle = function(a, randGen) {
 	var len = a.length;
 	for (var i = 0; i < len; ++i) {
@@ -52,6 +74,10 @@ exports.shuffle = function(a, randGen) {
 	}
 	return a;
 }
+
+/**
+ * Rotate an array's elements left.
+ */
 
 exports.rotate = function(a, count) {
 	var len = a.length,
