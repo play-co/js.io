03ba5e371c4db6b1ed605e1e9271227ab6038352
Arno van der Vegt <arno@gameclosure.com>
2012-10-12 10:40:43 -0700

    bug fixes

--- packages/math/geom/angle.js
+++ packages/math/geom/angle.js
@@ -1,42 +1,34 @@
-/**
- * @namespace
- */
-
 var PI = Math.PI;
-var TWOPI = Math.PI * 2;
+var TAU = Math.PI * 2;
 
 exports.average = function (a, b, weight) {
 	if (weight === undefined) { weight = 0.5; }
-	var r1 = exports.getRange(a, b);
-	var avg = r1 < PI 
+	var r1 = exports.range(a, b);
+	var avg = r1 < PI
 		? a + r1 * (1 - weight)
 		: b + (2 * PI - r1) * weight;
 
 	return avg > PI ? avg - 2 * PI : avg < -PI ? avg + 2 * PI : avg;
-}
+};
 
 // between -PI and PI
 exports.normalize = function (a) {
-	
-	// TODO: don't use loops
-	while(a < -PI) { a += 2 * PI; }
-	while(a > PI) { a -= 2 * PI; }
-	return a;
-}
+	return a - (((a + Math.PI) / TAU) | 0) * TAU;
+};
 
 exports.add = function (a, b) {
 	var sum = a + b;
-	return sum > PI ? sum - TWOPI : sum < -PI ? sum + TWOPI : sum;
-}
+	return sum > PI ? sum - TAU : sum < -PI ? sum + TAU : sum;
+};
 
 // smaller of two angles a - b, b - a
 exports.difference = function (a, b) {
-	var diff = exports.getRange(a, b);
-	return diff > PI ? diff - TWOPI : diff;
-}
+	var diff = exports.range(a, b);
+	return diff > PI ? diff - TAU : diff;
+};
 
 // angular range from a to b, returns float between [0, 2PI]
 exports.range = function (a, b) {
 	var r = b - a;
-	return r < 0 ? r + TWOPI : r;
-}
+	return r < 0 ? r + TAU : r;
+};
