0679764f1c179214c56ad52fff53808bb0ed2f47
Martin Hunt <martin@gameclosure.com>
2014-07-21 11:50:20 -0700

    update node_interface for onError

--- compilers/jsio_compile/node_interface.js
+++ compilers/jsio_compile/node_interface.js
@@ -40,21 +40,21 @@ exports.run = function(args, opts) {
 			args = result.args,
 			opts = result.opts;
 	}
-	
+
 	if (opts.help) {
 		usage();
 		process.exit();
 	}
-	
+
 	findMinifier(opts.closurePath);
-	
+
 	_compiler.run(args, opts);
 };
 
-exports.onError = function(opts, msg) {
+exports.onError = function(e) {
 	usage();
 	jsio.__env.log('');
-	logger.error('\n' + msg);
+	logger.error('\n', e);
 	jsio.__env.log('');
 	process.exit(1);
 }
@@ -72,16 +72,16 @@ exports.onFinish = function(opts, src) {
 
 exports.compress = function(filename, src, opts, callback) {
 	var cachePath;
-	
+
 	function fail(err) {
 		if (err) {
 			logger.error(err);
 		}
 		callback(src);
 	}
-	
+
 	if (!closurePath) { return fail(); }
-	
+
 	if (opts.compressorCachePath && filename) {
 		try {
 			var cacheFilename = (/^\.\//.test(filename) ? 'R-' + filename.substring(2) : 'A-' + filename)
@@ -98,7 +98,7 @@ exports.compress = function(filename, src, opts, callback) {
 				var stat = fs.statSync(filename);
 				var checksum = '' + stat.mtime;
 			}
-			
+
 			if (fs.existsSync(cachePath)) {
 				var cachedContents = fs.readFileSync(cachePath, 'utf8');
 				var i = cachedContents.indexOf('\n');
@@ -113,9 +113,9 @@ exports.compress = function(filename, src, opts, callback) {
 			logger.error(e);
 		}
 	}
-	
+
 	// http://code.google.com/p/closure-compiler/wiki/Warnings
-	
+
 	var spawn = require('child_process').spawn;
 	var cmd = ['-jar', closurePath || 'jsio_minify.jar', '--compilation_level', 'SIMPLE_OPTIMIZATIONS'];
 	if (opts.noIE) {
@@ -125,7 +125,7 @@ exports.compress = function(filename, src, opts, callback) {
 	var closure = spawn('java', cmd);
 	var stdout = [];
 	var stderr = [];
-	
+
 	closure.stdout.on('data', function(data) { stdout.push(data); });
 	closure.stderr.on('data', function(data) { stderr.push(data); });
 	closure.on('exit', function(code) {
@@ -139,13 +139,13 @@ exports.compress = function(filename, src, opts, callback) {
 			} catch(e) {
 				logger.error(e);
 			}
-			
+
 			callback(compressedSrc);
 		} else {
 			fail(stderr.join(''));
 		}
 	});
-	
+
 	closure.stdin.write(src);
 	closure.stdin.end();
 }
