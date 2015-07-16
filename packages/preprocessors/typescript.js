9319d3255956c3e7c2d6b562e017cd6d7e3945d5
Tom Fairfield <fairfield@cs.xu.edu>
2015-07-16 23:47:25 +0000

    switches to typestring for typescript compilation

--- packages/preprocessors/typescript.js
+++ packages/preprocessors/typescript.js
@@ -1,8 +1,21 @@
-var tsc = require('typescript-compiler');
+var ts = require('typestring');
+var fs = require('fs');
+
+var defCache = {};
+
+var readDefs = function(path) {
+  var results = fs.readdirSync(path);
+  results.filter(function(result) {
+    return !(result in defCache) && /\.d\.ts$/.test(result);
+  }).forEach(function(result) {
+    defCache[result] = fs.readFileSync(path + result, 'utf-8');  
+  });
+};
 
 exports = function (path, moduleDef, opts) {
   if (/\.ts$/.test(moduleDef.filename)) {
     //it is a typescript file
-    moduleDef.src = tsc.compileString(moduleDef.src);
+    readDefs(moduleDef.directory);
+    moduleDef.src = ts.compile(moduleDef.src, defCache);
   }
 };
