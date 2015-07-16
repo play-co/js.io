5ce8607c6b92b956b61369dcc84e368c674e7659
Tom Fairfield <fairfield@cs.xu.edu>
2015-07-16 20:57:18 +0000

    adds typescript-compiler to package.json
    This also removes some left over debug logging

--- packages/preprocessors/typescript.js
+++ packages/preprocessors/typescript.js
@@ -3,8 +3,6 @@ var tsc = require('typescript-compiler');
 exports = function (path, moduleDef, opts) {
   if (/\.ts$/.test(moduleDef.filename)) {
     //it is a typescript file
-    console.error(moduleDef.src);
     moduleDef.src = tsc.compileString(moduleDef.src);
-    console.error(moduleDef.src);
   }
 };
