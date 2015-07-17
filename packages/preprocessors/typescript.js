ef68755d321a0317cf2348a8591e073b16e23709
Tom Fairfield <fairfield@cs.xu.edu>
2015-07-17 21:49:38 +0000

    Correctly handles errors from compiling Typescript
    They get printed out in the same way `tsc` does and then correctly
    forwarded up to stop the compilation

--- packages/preprocessors/typescript.js
+++ packages/preprocessors/typescript.js
@@ -8,14 +8,26 @@ var readDefs = function(path) {
   results.filter(function(result) {
     return !(result in defCache) && /\.d\.ts$/.test(result);
   }).forEach(function(result) {
-    defCache[result] = fs.readFileSync(path + result, 'utf-8');  
+    defCache[result] = fs.readFileSync(path + result, 'utf-8');
   });
 };
 
-exports = function (path, moduleDef, opts) {
+exports = function (path, moduleDef) {
   if (/\.ts$/.test(moduleDef.filename)) {
     //it is a typescript file
     readDefs(moduleDef.directory);
-    moduleDef.src = ts.compile(moduleDef.src, defCache);
+    try {
+      moduleDef.src = ts.compile(moduleDef.src, defCache);
+    } catch (errors) {
+      console.error('Error compiling Typescript module:');
+      errors.forEach(function(error) {
+        var file = error.file;
+        var lineInfo = file.getLineAndCharacterFromPosition(error.start);
+        console.error(moduleDef.filename + '('
+                      + lineInfo.line + ',' + lineInfo.character  + '): '
+                      + 'error TS' + error.code +  ': ' +  error.messageText);
+      });
+      throw new Error('Typescript compile failed.');
+    }
   }
 };
