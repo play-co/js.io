663e04171a01d9ac86b741a8857312671c7c0d5e
Martin Hunt <martin@gameclosure.com>
2015-09-01 12:03:26 -0700

    custom setFile/setDirectory/setPath to keep all three in sync

--- packages/std/uri.js
+++ packages/std/uri.js
@@ -1,4 +1,6 @@
-var attrs = [ 
+import util.path;
+
+var attrs = [
 	"source",
 	"protocol",
 	"authority",
@@ -15,7 +17,7 @@ var attrs = [
 	"anchor"
 ];
 
-var URI = exports = Class(function(supr) {
+var URI = module.exports = Class(function() {
 	this.init = function(url, isStrict) {
 		if (url instanceof URI) {
 			for (var i = 0, attr; attr = attrs[i]; ++i) {
@@ -23,15 +25,15 @@ var URI = exports = Class(function(supr) {
 			}
 			return;
 		}
-		
+
 		this._isStrict = isStrict;
-		
+
 		var uriData = exports.parse(url, isStrict);
 		for (var attr in uriData) {
 			this['_' + attr] = uriData[attr];
-		};
-	}
-  
+		}
+	};
+
 	for (var i = 0, attr; attr = attrs[i]; ++i) {
 		(function(attr) {
 			var fNameSuffix = attr.charAt(0).toUpperCase() + attr.slice(1);
@@ -43,35 +45,51 @@ var URI = exports = Class(function(supr) {
 				return this;
 			};
 		}).call(this, attr);
-	};
-	
-	this.query = function(key) { return exports.parseQuery(this._query)[key]; }
-	this.hash = function(key) { return exports.parseQuery(this._anchor)[key]; }
-	
+	}
+
+	this.query = function(key) { return exports.parseQuery(this._query)[key]; };
+	this.hash = function(key) { return exports.parseQuery(this._anchor)[key]; };
+
 	this.addHash = function(kvp) {
 		var hash = exports.parseQuery(this._anchor);
 		for (var i in kvp) { hash[i] = kvp[i]; }
 		this._anchor = exports.buildQuery(hash);
 		return this;
-	}
-	
+	};
+
+	this.setPath = function (path) {
+		var pieces = util.path.splitPath(path);
+		this._file = pieces.filename;
+		this._directory = pieces.directory;
+		this._path = path;
+		return this;
+	};
+
+	this.setFile = function (file) {
+		return this.setPath(util.path.join(this._directory, file));
+	};
+
+	this.setDirectory = function (directory) {
+		return this.setPath(util.path.join(directory, this._file));
+	};
+
 	this.push = function(path) {
 		if (path) {
 			this._path = (this._path + '/' + path).replace(/\/\/+/g, '/');
 		}
 		return this;
-	}
-	
+	};
+
 	this.addQuery = function(kvp) {
 		var query = exports.parseQuery(this._query);
 		for (var i in kvp) { query[i] = kvp[i]; }
 		this._query = exports.buildQuery(query);
 		return this;
-	}
-	
+	};
+
 	this.removeQuery = function(keys) {
 		var query = exports.parseQuery(this._query);
-		if (isArray(keys)) {
+		if (Array.isArray(keys)) {
 			for (var i = 0, n = keys.length; i < n; ++i) {
 				delete query[keys[i]];
 			}
@@ -80,19 +98,19 @@ var URI = exports = Class(function(supr) {
 		}
 		this._query = exports.buildQuery(query);
 		return this;
-	}
+	};
 
-	this.toJSON = function() { return this.toString(false); }
+	this.toJSON = function() { return this.toString(false); };
 
 	this.toString = function(onlyBase) {
 		// XXX TODO: This is vaguely reasonable, but not complete. fix it...
-		var a = this._protocol ? this._protocol + "://" : ""
+		var a = this._protocol ? this._protocol + "://" : "";
 		var b = this._host ? this._host + ((this._port || 80) == 80 ? "" : ":" + this._port) : "";
-		
+
 		if (onlyBase) {
 			return a + b;
 		}
-		
+
 		var c = this._path;
 		var d = this._query ? '?' + this._query : '';
 		var e = this._anchor ? '#' + this._anchor : '';
@@ -113,17 +131,17 @@ exports.relativeTo = function(url, base) {
 	}
 
 	return new URI(url);
-}
+};
 
 exports.resolveRelative = function(url) {
 	var prevUrl;
-	
+
 	// remove ../ with preceeding folder
 	while((prevUrl = url) != (url = url.replace(/(^|\/)([^\/]+)\/\.\.\//g, '/'))) {};
-	
+
 	// remove ./ if it isn't preceeded by a .
 	return url.replace(/[^.]\.\//g, '');
-}
+};
 
 exports.buildQuery = function(kvp) {
 	var pairs = [];
@@ -131,7 +149,7 @@ exports.buildQuery = function(kvp) {
 		pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(kvp[key]));
 	}
 	return pairs.join('&');
-}
+};
 
 exports.parseQuery = function(str) {
 	var pairs = str.split('&'),
@@ -143,7 +161,7 @@ exports.parseQuery = function(str) {
 		if (key) { data[key] = decodeURIComponent(pair[1]); }
 	}
 	return data;
-}
+};
 
 // Regexs are based on parseUri 1.2.2
 // Original: (c) Steven Levithan <stevenlevithan.com>
@@ -160,16 +178,16 @@ exports.parse = function(str, isStrict) {
 	for (var i = 0, attr; attr = attrs[i]; ++i) {
 		result[attr] = match[i] || "";
 	}
-	
+
 	var qs = result['queryKey'] = {};
 	result['query'].replace(queryStringRegex, function(check, key, val) {
 		if (check) {
 			qs[key] = val;
 		}
 	});
-	
+
 	return result;
-}
+};
 
 exports.isSameDomain = function(urlA, urlB) {
 	var a = exports.parse(urlA);
